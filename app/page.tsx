d60fd6ea60eb1dc848572ae414089a05bca7c1cf
giannis198 <john_nik@windowslive.com>
2024-01-03 16:15:24 +0200

    text section resized

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import Image from "next/image";
 
 const page = () => {
   return (
-    <section className="md:bg-charcoal_gray h-screen md:pt-44">
-      <div className="md:w-[65%] mx-auto md:px-5 md:py-5 md:flex gap-14 items-center bg-white rounded-3xl">
+    <section className="md:bg-charcoal_gray h-screen md:pt-52">
+      <div className="md:w-[70%] mx-auto md:px-5 md:py-5 md:flex items-center justify-between bg-white rounded-3xl">
         <div className="md:hidden mx-auto pb-10 flex justify-center">
           <Image
             src="/illustration-sign-up-mobile.svg"
@@ -18,11 +18,11 @@ const page = () => {
             src="/illustration-sign-up-desktop.svg"
             alt="Front End Mentor"
             width={400}
-            height={400}
+            height={593}
           />
         </div>
 
-        <div className="text-charcoal_gray px-6 ">
+        <div className="text-charcoal_gray px-6 md:w-[47%] ">
           <h1 className="font-bold text-dark_slate_gray  text-5xl pb-2">
             Stay updated!
           </h1>
@@ -30,18 +30,18 @@ const page = () => {
             Join 60,000+ product managers receiving monthly updates on:
           </p>
 
-          <div className="flex gap-2 items-start pb-2">
+          <div className="flex gap-4 items-start pb-2">
             <Image
               src="/icon-list.svg"
               height={20}
               width={20}
               alt="list icon"
             />
-            <p className="text-charcoal_gray">
+            <p className="text-charcoal_gray flex-1">
               Product discovery and building what matters
             </p>
           </div>
-          <div className="text-charcoal_gray flex gap-2 items-start pb-2">
+          <div className="text-charcoal_gray flex flex-1 gap-4 items-start pb-2">
             <Image
               src="/icon-list.svg"
               height={20}
@@ -50,7 +50,7 @@ const page = () => {
             />
             <p>Measuring to ensure updates are a success</p>
           </div>
-          <div className="text-charcoal_gray flex gap-2 items-start pb-16">
+          <div className="text-charcoal_gray flex flex-1 gap-4 items-start pb-16">
             <Image
               src="/icon-list.svg"
               height={20}
