33f58d32032326ac66add2cf011cd09d1f797ca9
giannis198 <john_nik@windowslive.com>
2024-01-03 16:30:24 +0200

    text section 2:resized

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 const page = () => {
   return (
     <section className="md:bg-charcoal_gray h-screen md:pt-52">
-      <div className="md:w-[60%] mx-auto md:px-5 md:py-5 md:flex items-center justify-between bg-white rounded-3xl">
+      <div className="md:w-[63%] mx-auto md:px-5 md:py-5 md:flex items-center justify-between bg-white rounded-3xl">
         <div className="md:hidden mx-auto pb-10 flex justify-center">
           <Image
             src="/illustration-sign-up-mobile.svg"
